0564fda8346fcfa2b84831dca01d646bc1220c20
Megha Mapalagama <meghamapalagama@meghas-mbp.home>
2021-03-09 13:27:52 -0500

    second commit

--- src/welcome10/HelloWorld.java
+++ src/welcome10/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("welcome to Hello world 10!........");
+		
+		System.out.println("Hello! welcome to new Hello world 10!.........");
 
 	}
 
